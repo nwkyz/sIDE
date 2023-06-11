37451422787887e089c8f334381d80da4c2a425b
nwkyz <77189398+nwkyz@users.noreply.github.com>
2023-06-11 12:50:08 +0800

    pushed alpha B2K542 --Theme changes and executable fixes

--- lua/plugins/themeswitch.lua
+++ lua/plugins/themeswitch.lua
@@ -56,7 +56,7 @@ vim.cmd.colorscheme "catppuccin" ]]--
 require("tokyonight").setup({
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+    style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
     light_style = "day", -- The theme is used when the background is set to light
     transparent = false, -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
@@ -72,7 +72,7 @@ require("tokyonight").setup({
         -- floats = "dark", -- style for floating windows
     },
     sidebars = { "qf", "help", "packer" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-    day_brightness = 0.4, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+    --day_brightness = 0.4, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
     hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
     dim_inactive = false, -- dims inactive windows
     lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
@@ -80,7 +80,11 @@ require("tokyonight").setup({
     --- You can override specific color groups to use other groups or a hex color
     --- function will be called with a ColorScheme table
     ---@param colors ColorScheme
-    on_colors = function(colors) end,
+    --[[on_colors = function(colors)
+    colors.bg = '#242843'
+    colors.bg_sidebar = '#1E1F37'
+    colors.bg_statusline = '#242843'
+    end,]]--
 
     --- You can override specific highlights to use other groups or a hex color
     --- function will be called with a Highlights and ColorScheme table
